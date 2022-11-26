bc6351e053b63a2f3a93e5172d6f74c62e5e5c23
susantlalshrestha <susantlalshrestha@gmail.com>
2022-11-26 12:10:24 -0500

    added: code to handle notification in application

--- src/application.tsx
+++ src/application.tsx
@@ -1,3 +1,5 @@
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AlgoliaProvider } from "./services/algolia";
 import { AppCache, AppClient, AppClientProvider } from "./services/graphql";
@@ -6,6 +8,17 @@ import { AppNavigation } from "./navigation";
 import React from "react";
 import { ResourcesProvider } from "./resources";
 import { AuthContextProvider } from "./contexts/auth";
+import { Platform } from "react-native";
+import { Subscription } from "expo-modules-core";
+import { Notification } from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export function Application() {
   const algoliaApiKey = "6c2c5bb09c6f0da1002a51d1995969bd";
@@ -14,6 +27,38 @@ export function Application() {
   const apiUri = "https://dev-api.pocketseq.com/dev/graphql";
   const appCache = new AppCache({ storage: AsyncStorage });
   const appClient = new AppClient({ cache: appCache, uri: apiUri });
+
+  const [expoPushToken, setExpoPushToken] = React.useState<
+    string | undefined
+  >();
+  const [notification, setNotification] = React.useState<Notification>();
+  const notificationListener = React.useRef<Subscription>();
+  const responseListener = React.useRef<Subscription>();
+
+  React.useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (notificationListener.current)
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      if (responseListener.current)
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   return (
     <AppClientProvider client={appClient}>
@@ -27,4 +72,37 @@ export function Application() {
       </AlgoliaProvider>
     </AppClientProvider>
   );
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  return token;
 }
